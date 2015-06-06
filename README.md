2e2f83731640b60d68d2a6eff202f2f3a5f7733a
Axelandre42 <axelandre42@yahoo.fr>
2015-06-06 15:09:01 +0200

    - updating README.md - creating a Block (Locomotive Relay)
    Signed-off-by: Axelandre42 <axelandre42@yahoo.fr>

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Adds various things that works with OpenComputers
 
 ### Features :
 * _TODO (5%)_ Railcraft's Locomotive Control.
+* _TODO (0%)_ 
+
+### Contributing :
+Ask me !
--- src/main/java/net/axelandre42/mct/common/block/BlockLocomotiveRelay.java
+++ src/main/java/net/axelandre42/mct/common/block/BlockLocomotiveRelay.java
@@ -1,13 +1,22 @@
 package net.axelandre42.mct.common.block;
 
+import net.axelandre42.mct.CreativeTab;
+import net.axelandre42.mct.MoreCompuThings;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class BlockLocomotiveRelay extends Block {
 
-	protected BlockLocomotiveRelay() {
+	public BlockLocomotiveRelay() {
 		super(Material.rock);
 		// TODO Auto-generated constructor stub
+		this.setCreativeTab(CreativeTab.INSTANCE);
+		this.setHardness(0.5F);
+		this.setBlockName("locomotive_relay");
+		this.setBlockTextureName(MoreCompuThings.MODID + ":locomotive_relay");
+		
 	}
+	
+	
 
 }
